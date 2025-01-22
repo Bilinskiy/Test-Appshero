de7ff2b6c77082bf539c343e97314413d87f95d8
bilinskiy <bilinskijwork@gmail.com>
2025-01-22 19:06:17 +0300

    Initial Commit

---
+++ TestAppshero/TestAppsheroApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppsheroApp.swift
+//  TestAppshero
+//
+//  Created by Дмитрий Билинский on 22.01.25.
+//
+
+import SwiftUI
+
+@main
+struct TestAppsheroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
